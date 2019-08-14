ee922b3f88609846a470cd8d11aa4b31537ef604
prakhar211 <gupta.prakhar96@gmail.com>
2019-08-14 16:38:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This sample script creates a web app in App Service with its related resources, and then deploys your web app code from a local Git repository."
+   return render_template("image.html")
