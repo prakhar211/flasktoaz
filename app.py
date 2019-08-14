fbc0e3b02005bdcd63097cee64b4fb1d2aa4e727
prakhar211 <gupta.prakhar96@gmail.com>
2019-08-14 16:27:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template(<img src = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1MmB8?ver=6c43g")
+    return render_template("image.html")
